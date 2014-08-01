14c49eab8c9b0cb43581bb3cc518324a3188d535
Stephan Uhle <stephanu@gmx.net>
2014-08-01 10:18:14 +0200

    Switched to openstreetmap tiles since cloudmade has shut down its map services.

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -31,9 +31,7 @@ $(function() {
 	// initialize leaflet map
 	var map = L.map("map").setView(position, zoom);
 
-	// you need a cloudmade apikey for adding the openstreetmap layer, get one at http://cloudmade.com/register
-	var leafletApiKey = "a31fc30597ec4aabbb753446a8cbf0d6"
-	L.tileLayer('http://{s}.tile.cloudmade.com/' + leafletApiKey + '/997/256/{z}/{x}/{y}.png', {
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     	maxZoom: 18
 	}).addTo(map);
